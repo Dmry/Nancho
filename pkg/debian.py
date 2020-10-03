546c826cd6a2bac7055274e45c63256ab48931af
Daniël <daniel.emmery@protonmail.com>
2020-10-03 14:31:47 +0200

    Generate .deb package for nancho

---
+++ pkg/debian.py
@@ -0,0 +1,51 @@
+#!/bin/python
+
+from pathlib import Path
+from shutil import copyfile, copytree, rmtree
+from os import system, chmod, remove, rmdir
+import stat
+import argparse
+
+parser = argparse.ArgumentParser(description='Build .deb package after compiling nancho.')
+parser.add_argument('--clean', action='store_true')
+
+target_dir = "debian_package"
+package_name = "nancho-0.0.2.deb"
+
+directory_tree = [
+    "usr/local/bin",
+    "etc/systemd/user"
+]
+
+targets = [
+    ("../build/src/nancho", "usr/local/bin/nancho"),
+    ("../resources/nancho.service", "etc/systemd/user/nancho.service")
+]
+
+def main():
+    args = parser.parse_args()
+
+    if args.clean == True:
+        try:
+            rmtree(target_dir)
+        except:
+            pass
+
+        try:
+            remove(package_name)
+        except:
+            pass
+    else:
+        for path_str in directory_tree:
+            Path(f'{target_dir}/{path_str}').mkdir(parents=True, exist_ok=True)
+
+        copytree("DEBIAN", f'{target_dir}/DEBIAN')
+
+        for source, target in targets:
+            copyfile(source, f'{target_dir}/{target}')
+
+        chmod(f"{target_dir}/usr/local/bin/nancho", stat.S_IRWXU | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH)
+        system(f'dpkg-deb --build ./{target_dir} {package_name}')
+
+if __name__ == "__main__":
+    main()
